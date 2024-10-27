b9566da5f04bc878ff620151af2622586f9996d0
Haftamu Kebede <54979169+haftamuk@users.noreply.github.com>
2024-10-27 19:46:19 +0300

    package module setting

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const net = require("net");
+import net from "net";
 
-var gps = require("gps-tracking");
+import gps from "gps-tracking";
+import BinaryStream from "@jsprismarine/jsbinaryutils";
 
 var options = {
   debug: true, //We don't want to debug info automatically. We are going to log everything manually so you can check what happens everywhere
@@ -8,6 +9,23 @@ var options = {
   device_adapter: "GT06",
 };
 
+/**
+ * packetLen
+ * Message_Head
+ * Message_body
+ *
+ * 7e010000390202081740490688000000003131313131544d4b4a2d41303100000000000000000000000038313734303439000000000000000000000000000000000000000000d37e
+ * 7e
+ * 01
+ * 02
+ * 00
+ * 06
+ * 02
+ * 02
+ * 08
+ * 17404907f5020208174049f77e
+ *
+ */
 var server = gps.server(options, function (device, connection) {
   function bufferToHexString(buffer) {
     var str = "";
@@ -24,9 +42,16 @@ var server = gps.server(options, function (device, connection) {
   connection.on("data", function (data) {
     //echo raw data package
     console.log("======================================");
-    console.log("UT04S RAW DATA EMITTED : " + bufferToHexString(data));
+    console.log("UT04S RAW DATA EMITTED HEX : " + data.toString('hex'));
+    console.log("UT04S RAW DATA EMITTED UTF8 : " + data.toString('utf8'));
+
+    console.log("size of a buffer (in bytes) : " + data.length);
+
+    const stream = new BinaryStream(data);
+    console.log("CMD TYPE UT04S : " + stream.readUnsignedShortLE());
+
     console.log("======================================");
 
-    console.log("Connection Obj: " + Object.toString(connection));
+    // console.log("Connection Obj: " + Object.toString(connection));
   });
 });
