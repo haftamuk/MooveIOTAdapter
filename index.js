1046e7835a14742ecd39b74a300c01496ffa8304
Haftamu Kebede <54979169+haftamuk@users.noreply.github.com>
2024-10-27 20:08:21 +0300

    package module setting

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ var server = gps.server(options, function (device, connection) {
 
     console.log("size of a buffer (in bytes) : " + data.length);
 
-    const stream = new BinaryStream(data);
+    const stream = new BinaryStream(Buffer.from(data));
     console.log("CMD TYPE UT04S : " + stream.readUnsignedShortLE());
 
     console.log("======================================");
