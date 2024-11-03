009dd86a8ed0b59869883e8dbf0f063c15d5f7fc
Haftamu Kebede <54979169+haftamuk@users.noreply.github.com>
2024-11-03 10:27:13 +0300

    UT04S Adaptor basic created

--- index.js
+++ index.js
@@ -1,109 +1,59 @@
 import net from "net";
 
 import gps from "gps-tracking";
-import BinaryStream from "@jsprismarine/jsbinaryutils";
-import { isUint16Array, isUint32Array, isUint8Array } from "util/types";
-import BitConverter from "bit-converter";
+// import BinaryStream from "@jsprismarine/jsbinaryutils";
+// import { isUint16Array, isUint32Array, isUint8Array } from "util/types";
+// import BitConverter from "bit-converter";
 
 var options = {
   debug: true, //We don't want to debug info automatically. We are going to log everything manually so you can check what happens everywhere
   port: 8800,
-  device_adapter: "GT06",
+  device_adapter: "UT04S",
 };
 
-const MSG_UPLOAD_POSITION = 0x0008;
-const MSG_UPLOAD_POSITION_NEW = 0x0032;
-const MSG_CONTROL = 0x0002;
-const MSG_CONTROL_RESPONSE = 0x8009;
-const MSG_ALARM = 0x0003;
-const MSG_SHAKE_HAND = 0x0000;
-const MSG_SHAKE_HAND_RESPONSE = 0x8000;
-const MSG_IMAGE_SIZE = 0x0200;
-const MSG_IMAGE_PACKET = 0x0201;
-
-/**
- * packetLen
- * Message_Head
- * Message_body
- *
- * 7e010000390202081740490688000000003131313131544d4b4a2d41303100000000000000000000000038313734303439000000000000000000000000000000000000000000d37e
- * 7e
- * 01
- * 02
- * 00
- * 06
- * 02
- * 02
- * 08
- * 17404907f5020208174049f77e
- *
+/***
+ * https://stackoverflow.com/questions/38931866/convert-gps-position-from-double-value
  */
+
 var server = gps.server(options, function (device, connection) {
-  function bufferToHexString(buffer) {
-    var str = "";
-    for (var i = 0; i < buffer.length; i++) {
-      if (buffer[i] < 16) {
-        str += "0";
-      }
-      str += buffer[i].toString(16);
-    }
-    return str;
-  }
+  device.on("connected", function (data) {
+    console.log("I'm a new ut04s device connected");
+    return data;
+  });
+
+  device.on("login_request", function (device_id, msg_parts) {
+    console.log(
+      "Hey! I want to start transmiting my position. Please accept me. My name is " +
+        device_id
+    );
+
+    this.login_authorized(true);
+
+    console.log("Ok, " + device_id + ", you're accepted!");
+  });
+
+  device.on("ping", function (data) {
+    //this = device
+    console.log(
+      "I'm here: " +
+        data.latitude +
+        ", " +
+        data.longitude +
+        " (" +
+        this.getUID() +
+        ")"
+    );
+
+    //Look what informations the device sends to you (maybe velocity, gas level, etc)
+    //console.log(data);
+    return data;
+  });
 
   //Also, you can listen on the native connection object
   connection.on("data", function (data) {
     //echo raw data package
     console.log("======================================");
-    console.log("UT04S RAW DATA EMITTED HEX : " + data.toString("hex"));
-    console.log("UT04S RAW DATA EMITTED UTF8 : " + data.toString("utf8"));
+    console.log("UT04S RAW DATA HEX : " + data.toString("hex"));
 
-    console.log("size of a buffer (in bytes) : " + data.length);
-
-    const stream = new BinaryStream(data.slice(2));
-    console.log("SLICE AT 4 : ");
-    console.log(data.slice(2).toString("hex"));
-
-    console.log("CMD TYPE UT04S");
-
-    // console.log(stream.read(data.length));
-
-    console.log(stream.readUnsignedShortLE());
-
-    console.log("======================================");
-
-    isUint16Array(data)
-      ? console.log("isUint16Array")
-      : console.log("NOT isUint16Array");
-
-    isUint32Array(data)
-      ? console.log("isUint32Array")
-      : console.log("NOT isUint32Array");
-
-    isUint8Array(data)
-      ? console.log("isUint8Array")
-      : console.log("NOT isUint8Array");
-
-    // const buf = new Buffer(data);
-
-    let packetLen = new Uint16Array(data.buffer, 0, 1);
-
-    let cmd = new Uint16Array(data.buffer, 2, 1);
-
-    console.log("packetLen : ");
-    console.log(packetLen);
-
-    console.log("cmd : ");
-    console.log(cmd);
-
-    const a = BitConverter.toShort([0xff, 0xff]);
-
-    console.log(a); // -1
-
-    //Starting index for the array = 1
-    const b = BitConverter.toShort([0, 0xff, 0xff], 1);
-
-    console.log(b); // -1
-
-    // console.log("Connection Obj: " + Object.toString(connection));
   });
 });
