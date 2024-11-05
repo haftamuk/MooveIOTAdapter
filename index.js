33b57090b0f7758e78996e364a1ba542ac8817c8
Haftamu Kebede <54979169+haftamuk@users.noreply.github.com>
2024-11-05 06:02:45 +0300

    Response is sent for authentication and location reports comming from terminal

--- index.js
+++ index.js
@@ -20,7 +20,23 @@ var server = gps.server(options, function (device, connection) {
     console.log("I'm a new ut04s device connected");
     return data;
   });
+  device.on("new_device_first_time", function (device_id, msg_parts) {
+    console.log("NEW DEVICE EVER DETECTED: " + device_id);
 
+    this.receive_first_time(msg_parts);
+  });
+
+  device.on("hbt", function (device_id, msg_parts) {
+    console.log("HEARTBEAT RECEIVED: " + device_id);
+
+    this.receive_hbt(msg_parts);
+  });
+
+  device.on("register", function (device_id, msg_parts) {
+    console.log("TERMINAL TRYING TO REGISTER: " + device_id);
+
+    this.new_device_register(msg_parts);
+  });
   device.on("login_request", function (device_id, msg_parts) {
     console.log(
       "Hey! I want to start transmiting my position. Please accept me. My name is " +
@@ -31,8 +47,12 @@ var server = gps.server(options, function (device, connection) {
 
     console.log("Ok, " + device_id + ", you're accepted!");
   });
+  device.on("logout", function (device_id, msg_parts) {
+    console.log("TERMINAL TRYING TO LOGOUT: " + device_id);
+    this.logout(msg_parts);
+  });
 
-  device.on("ping", function (data) {
+  device.on("ping", function (data, msg_parts) {
     //this = device
     console.log(
       "I'm here: " +
@@ -43,10 +63,21 @@ var server = gps.server(options, function (device, connection) {
         this.getUID() +
         ")"
     );
+    this.received_location_report(msg_parts);
 
     //Look what informations the device sends to you (maybe velocity, gas level, etc)
     //console.log(data);
     return data;
+  });
+
+  device.on("alarm", function (alarmData, msgParts) {
+    //this = device
+    console.log("TODO- ALARM PARSING");
+    //Look what informations the device sends to you (maybe velocity, gas level, etc)
+    //console.log(data);
+    this.received_alarm_report(msgParts);
+
+    return msgParts;
   });
 
   //Also, you can listen on the native connection object
@@ -54,6 +85,5 @@ var server = gps.server(options, function (device, connection) {
     //echo raw data package
     console.log("======================================");
     console.log("UT04S RAW DATA HEX : " + data.toString("hex"));
-
   });
 });
