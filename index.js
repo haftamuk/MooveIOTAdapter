c5c90018134530a1587734487570209a22fde740
Haftamu Kebede <54979169+haftamuk@users.noreply.github.com>
2024-11-15 18:19:53 +0300

    script npm start added to package.json

--- index.js
+++ index.js
@@ -88,13 +88,17 @@ var server = gps.server(options, function (device, connection) {
     fetch(`http://78.47.144.132:3000/api/GPSLocationFeed`, {
       method: "POST",
       mode: "cors",
-      body: JSON.stringify({ data: data }),
+      body: JSON.stringify({ data: msg_parts.data }),
       headers: {
         "Content-Type": "application/json; charset=UTF-8",
       },
     })
       .then((response) => response.json())
-      .then((data) => console.log("MooveLocation Returned Data : ", data));
+      .then((data) => console.log("MooveLocation Returned Data : ", data))
+      .catch((err) => {
+        console.log("ERROR SENDING TO MOOVE LOCATION PLATFORM");
+        console.log(err);
+      });
     /**
      * #######################################################################
      */
