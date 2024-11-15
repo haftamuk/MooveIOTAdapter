13184a9d9d8789ce09c368e40b54a7a2f8b059ce
Haftamu Kebede <54979169+haftamuk@users.noreply.github.com>
2024-11-15 18:38:54 +0300

    script npm start added to package.json

--- index.js
+++ index.js
@@ -88,7 +88,7 @@ var server = gps.server(options, function (device, connection) {
     fetch(`http://78.47.144.132:3000/api/GPSLocationFeed`, {
       method: "POST",
       mode: "cors",
-      body: JSON.stringify({ data: data }),
+      body: JSON.stringify(data),
       headers: {
         "Content-Type": "application/json; charset=UTF-8",
       },
@@ -100,8 +100,8 @@ var server = gps.server(options, function (device, connection) {
         console.log(err);
         console.log("data OBJECT");
         console.log(data);
-        console.log("JSON.stringify({ data: data })");
-        console.log(JSON.stringify({ data: data }));
+        console.log("JSON.stringify(data)");
+        console.log(JSON.stringify(data));
       });
     /**
      * #######################################################################
